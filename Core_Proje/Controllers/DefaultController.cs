4236085c977ebe018a58b3878c42d767280e3256
Onur Abdulaji <76725341+onurabdulaji@users.noreply.github.com>
2023-08-20 02:48:43 +0200

    Add project files.

---
+++ Core_Proje/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}
