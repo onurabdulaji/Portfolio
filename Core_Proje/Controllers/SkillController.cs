3b45d657e1322890275a1a0c737ba64d5f63d06e
Onur Abdulaji <76725341+onurabdulaji@users.noreply.github.com>
2023-08-25 14:16:23 +0200

    Admin Template

--- Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Controllers/SkillController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
@@ -9,8 +10,25 @@ namespace Core_Proje.Controllers
         SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            ViewBag.v1 = "Yetenek Listesi";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Listesi";
             var values = skillManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            ViewBag.v1 = "Yetenek Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
         }
     }
 }
