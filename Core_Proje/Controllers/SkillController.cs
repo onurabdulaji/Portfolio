8c90627d2fdf55b0b147b4c87daaa541dd988b90
Onur Abdulaji <76725341+onurabdulaji@users.noreply.github.com>
2023-08-28 09:12:35 +0200

    # Admin Skills CRUD

--- Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Controllers/SkillController.cs
@@ -30,5 +30,26 @@ namespace Core_Proje.Controllers
             skillManager.TAdd(skill);
             return RedirectToAction("Index");
         }
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = skillManager.TGetByID(id);
+            skillManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditSkill(int id)
+        {
+            ViewBag.v1 = "Yetenek Guncelleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Guncelleme";
+            var values = skillManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            skillManager.TUpdate(skill);
+            return RedirectToAction("Index");
+        }
     }
 }
