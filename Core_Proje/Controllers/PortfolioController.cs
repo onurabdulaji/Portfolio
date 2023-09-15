77405cb0b8b89d3fdb7b0f368f4d6e088525d6d3
Onur Abdulaji <76725341+onurabdulaji@users.noreply.github.com>
2023-09-15 12:59:49 +0200

    # Portflio CRUD %70

---
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        //Managerden bir manager uretmek 
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            //Listeleme Metodu 
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/20230915104906_mig_add_project_columns_portfolio.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_add_project_columns_portfolio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image1",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image2",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image3",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image4",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Platfrom",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Price",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Portfolios",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Value",
+                table: "Portfolios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image1",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Image2",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Image3",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Image4",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Platfrom",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "Portfolios");
+        }
+    }
+}
