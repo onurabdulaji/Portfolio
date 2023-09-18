50adaeb8ce9cd8694edbcae5b7b8eea394ad21e5
Onur Abdulaji <76725341+onurabdulaji@users.noreply.github.com>
2023-09-18 02:53:55 +0200

    # Dashobaard and New Theme

---
+++ Core_Proje/ViewComponents/Dashboard/Last5Projects.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class Last5Projects : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
