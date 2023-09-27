66a08fc450217dfc2ae529c4027ba32066459c67
Onur Abdulaji <76725341+onurabdulaji@users.noreply.github.com>
2023-09-27 13:15:40 +0200

    # Dashboard Weather API

---
+++ Core_Proje/Areas/Writer/Controllers/DashboardWriterController.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class DashboardWriterController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardWriterController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.Surname;
+
+            //WeatherAPI
+            string api = "8c7b6044784afce91a5f9ddd287ddcdf";
+            string connection =
+                "https://api.openweathermap.org/data/2.5/weather?q=London&mode=xml&units==metric&appid=8c7b6044784afce91a5f9ddd287ddcdf"
+                + api;
+            XDocument documentdoc = XDocument.Load(connection);
+            ViewBag.v5 = documentdoc.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            //Statistics
+            Context c = new Context();
+            ViewBag.v1 = 0;
+            ViewBag.v2 = c.Announcements.Count();
+            ViewBag.v3 = 0;
+            ViewBag.v4 = c.Skills.Count();
+            return View();
+        }
+    }
+}
+
+//https://api.openweathermap.org/data/2.5/weather?q=London&mode=xml&units==metric&appid=8c7b6044784afce91a5f9ddd287ddcdf
---
+++ Core_Proje/Areas/Writer/ViewComponents/Notification.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Core_Proje.Areas.Writer.ViewComponents
+{
+    public class Notification : ViewComponent
+    {
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var value = announcementManager.TGetList().Take(5).ToList();
+            return View(value);
+        }
+    }
+}
